72fea4f5330ff6c7f24d2fb22e8de71989620b9c
Oleg <pomidoroff@list.ru>
2021-05-23 21:56:26 +0300

    1-division

---
+++ lesson-03/1_Division.py
@@ -0,0 +1,33 @@
+# GeekBrains > Python basics: Oleg Gladkiy (https://geekbrains.ru/users/3837199)
+homework_type = "Lesson-3. 1-Division"
+# 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
+#    Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+
+print(homework_type)
+
+
+def str_to_digit(number_as_string):
+    try:
+        my_number = float(number_as_string)
+    except ValueError:
+        my_number = None
+        print("Digital Error")
+    return my_number
+
+
+def my_func_division(a, b):
+    if b == 0:
+        print("Ошибка делителя: на ноль делить нельзя")
+        return None
+    return a / b
+
+
+number_a = str_to_digit(input("Ведите 1-ое число: "))
+if number_a != None:
+    number_b = str_to_digit(input("Ведите 2-ое число: "))
+    if number_b != None:
+        result_number = my_func_division(number_a, number_b)
+        if result_number != None:
+            print(f"Результат деления: {result_number}")
+
+print("End")
