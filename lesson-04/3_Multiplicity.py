983e401ed126a40ad7bd5102952934c71b8e57a0
Oleg <pomidoroff@list.ru>
2021-05-30 00:23:42 +0300

    3. Multiplicity ready

---
+++ lesson-04/3_Multiplicity.py
@@ -0,0 +1,14 @@
+# GeekBrains > Python basics: Oleg Gladkiy (https://geekbrains.ru/users/3837199)
+homework_type = "Lesson-4. 3-Multiplicity"
+# 3. Для чисел в пределах от 20 до 240 найти числа, кратные 20 или 21. Необходимо решить задание в одну строку.
+#    Подсказка: использовать функцию range() и генератор.
+
+print(homework_type)
+
+source_number_list = [i for i in range(20, 241, 1)]
+multiplicity_numbers = [i for i in source_number_list if i % 20 == 0 or i % 21 == 0]
+
+print(source_number_list)
+print(multiplicity_numbers)
+
+print("End")
