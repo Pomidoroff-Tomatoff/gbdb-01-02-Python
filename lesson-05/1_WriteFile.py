30884741ba1f6c0feb3bf605b4a7b9382debaf15
Oleg <pomidoroff@list.ru>
2023-04-03 03:13:07 +0300

    01_Write fix

--- lesson-05/1_WriteFile.py
+++ lesson-05/1_WriteFile.py
@@ -5,17 +5,17 @@ homework_type = "Lesson-5. 1-WriteFile"
 
 print(homework_type)
 
-with open("1_WriteFile_DATA_program.txt", "w", encoding="utf-8") as data_file:
+with open("1_WriteFile_DATA_program_new.txt", "w", encoding="utf-8") as data_file:
 
-    print("Введите строку данных или Enter для звершения: ")
+    print("Введите строку данных или Enter для завершения: ")
 
     line_count_max = 10
     line_count = 0
 
     while line_count < line_count_max:
         line_count += 1
-        in_str = input(f"Line {line_count: 2d}>> ")
-        if in_str == "" or in_str == " ":  # проверяем на пустую строку или пробел (только один)
+        in_str = input(f"Line {line_count: 2d}>> ").rstrip()  # и удаляем пробелы справа
+        if not in_str or in_str.isspace():  # проверяем на пустую строку или пробелы (в том числе \t, \n и так далее...)
             break
         else:
             print(in_str, file=data_file)
