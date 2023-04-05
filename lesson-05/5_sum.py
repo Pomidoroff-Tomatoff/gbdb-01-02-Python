99f22c3e9bb4aa424f3a09e45b5c173976583a2f
Oleg <pomidoroff@list.ru>
2023-04-05 16:05:01 +0300

    5_sum: fix smaller

--- lesson-05/5_sum.py
+++ lesson-05/5_sum.py
@@ -1,5 +1,5 @@
 # GeekBrains, Python basics: Oleg Gladkiy (https://geekbrains.ru/users/3837199)
-homework_type = "Lesson-5. 5-sum-ALL: пишем и читаем числа сразу всем списком"
+homework_type = "Lesson-5. 5-sum-ALL: МАССОВЫЕ ОПЕРАЦИИ: генерация, запись и чтение"
 ''' Задание №:
 5.  Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
     Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
@@ -13,39 +13,41 @@ print("\n" + homework_type + "\n")
 
 import os, sys, random, time
 
-NUMBERS_AMOUNT_MAX = 100000                           # Количество чисел
+NUMBERS_AMOUNT_MAX = 1000000                          # Количество чисел
 SEPARATOR = " "                                       # Разделитель чисел: " ", ",", ";", ";"
 module_name = os.path.basename(sys.argv[0])           # Имя этого модуля...
 module_name = "".join(module_name.split(".py")[:-1])  # ...и без расширения
-DATA_FILENAME = module_name + "_numbers.txt"          # Имя файла данных
+DATA_FILENAME = module_name + "__numbers.txt"          # Имя файла данных
 
 
-def gen_numbers(count=NUMBERS_AMOUNT_MAX):
-    ''' Генератор чисел '''
-    for i in range(count):
-        yield round(random.uniform(0, 100), 2)
-
-
-# Пишем.
+# Генерируем и Пишем
 with open(DATA_FILENAME, "w+") as file_out:
-    '''Создаём список, объединяем его в строку и сразу пишем в файл на диск'''
+    '''Генерируем список чисел, объединяем его в строку и пишем в файл на диск'''
+
+    # Генерируем числа
     time_start = time.time()
-    numbers = list(gen_numbers())  # список всех чисел
-    print(f"{SEPARATOR.join(map(str, numbers))}", end="", flush=True, file=file_out)
+    numbers = [random.uniform(0, 100) for _ in range(NUMBERS_AMOUNT_MAX)]
+    # >>>
+    print(f"\rГенер.: {len(numbers)/NUMBERS_AMOUNT_MAX:3.0%}   {len(numbers):,d}   сумма: {sum(numbers):f}   время: {(time.time() - time_start):.2f} с   объём {(sys.getsizeof(numbers)):,d} байт ", end='\n')
 
-    # >>> на экран:
-    print(f"Запись: {len(numbers)/NUMBERS_AMOUNT_MAX:3.0%}   {len(numbers):,d}   сумма: {sum(numbers):.2f}   время: {(time.time() - time_start):.2f} с")
+    # Пишем
+    print(f"\rЗапись...", end=""*5)
+    time_start = time.time()
+    print(f"{SEPARATOR.join(map(lambda x: f'{x:f}', numbers))}", end="", flush=True, file=file_out)  # str() в 2-а раза медленее...
 
-# Читаем и считаем
+    # >>> экран
+    print(f"\rЗапись: {len(numbers)/NUMBERS_AMOUNT_MAX:3.0%}   {len(numbers):,d}   сумма: {sum(numbers):f}   время: {(time.time() - time_start):.2f} с  ", end='\n')
+
+# Читаем и суммируем
 with open(DATA_FILENAME, "r") as file_in:
-    '''Читаем сразу всё. Так же вычисляем. Память не экономим'''
+    '''Читаем сразу всё. Потом вычисляем. Оперативную память не экономим'''
+    print(f"\rЧтение ...", end=""*5)
     time_start = time.time()
     numbers = []
     for line in file_in:
         for element in line.split(SEPARATOR):
             if element:
-                number = round(float(element), 2)
+                number = float(element)
                 numbers.append(number)
-
-    # >>> на экран:
-    print(f"Чтение: {len(numbers)/NUMBERS_AMOUNT_MAX:3.0%}   {len(numbers):,d}   сумма: {sum(numbers):.2f}   время: {(time.time() - time_start):.2f} с")
+    # >>> экран
+    print(f"\rЧтение: {len(numbers)/NUMBERS_AMOUNT_MAX:3.0%}   {len(numbers):,d}   сумма: {sum(numbers):f}   время: {(time.time() - time_start):.2f} с   объём {sys.getsizeof(numbers):,d} байт")
--- lesson-05/5_sum_single.py
+++ lesson-05/5_sum_single.py
@@ -4,27 +4,27 @@ homework_type = "Lesson-5. 5-sum-ALL: индивидуальная запись-
 5.  Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
     Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
     Решение:
-    -- Пишем и читаем каждое число индивидуально.
-    -- Особенности: 
+    -- Пишем и читаем каждое число индивидуально:
        > очень экономное использование памяти -- только на одно число;
        > можно обработать очень большие объёмы чисел, ограничиваясь только дисковым пространством 
        > низкая скорость, которая сильно зависит от кэша дисковой системы
-       > работает открыто информируя о состоянии дел в текущий момент времени '''
+       > работает открыто информируя о состоянии проведения операций в каждый момент времени
+         (процедура не создаёт видимости зависания отсутствием информации) '''
 print("\n" + homework_type + "\n")
 
 import os, sys, random, time
 
-NUMBERS_AMOUNT_MAX = 100000                          # Количество чисел
+NUMBERS_AMOUNT_MAX = 1000000                          # Количество чисел
 SEPARATOR = " "                                       # Разделитель чисел: " ", ",", ";", ";"
 module_name = os.path.basename(sys.argv[0])           # Имя этого модуля...
 module_name = "".join(module_name.split(".py")[:-1])  # ...и без расширения
-DATA_FILENAME = module_name + "_numbers.txt"          # Имя файла данных
+DATA_FILENAME = module_name + "__numbers.txt"          # Имя файла данных
 
 
 def gen_numbers(count=NUMBERS_AMOUNT_MAX):
     ''' Генератор чисел '''
     for i in range(count):
-        yield round(random.uniform(0, 100), 2)
+        yield random.uniform(0, 100)               # Внимание: округление замедляем процесс...
 
 
 def get_number(file_name=DATA_FILENAME):
@@ -32,18 +32,15 @@ def get_number(file_name=DATA_FILENAME):
     global SEPARATOR
     with open(file_name, "r") as file_in:
         symbols = []
-        while symbol := file_in.read(1):
-            if symbol == SEPARATOR or symbol in "\n\r":
-                number_str = "".join(symbols).strip()  # удаляем граничные пробелы или вообще, если больше ничего нет
-                if number_str and not number_str.isspace():
-                    yield round(float(number_str), 2)
-                else:
-                    pass  # Пустая строка!
-                symbols = []
-            else:
-                symbols.extend(symbol)
-        else:
-            return None
+        while symbol := file_in.read(1):                     # читаем символы числа, пока нет EOF==''
+            if symbol == SEPARATOR or symbol in "\n\r":      # разделитель: число завершено
+                number_str = "".join(symbols).strip()        # собираем число в строку без пробелов
+                symbols = []                                 # Сбрасываем набор символов считанного числа
+                if number_str and not number_str.isspace():  # если это не пустая строка
+                    yield float(number_str)                  # ВОЗВРАЩАЕМ ЧИСЛО
+            else:                                            # Собираем символы числа
+                symbols.extend(symbol)                       # в список
+    return None                                              # работа генератора завершена
 
 
 # Пишем по одному сгенерированному числу
@@ -53,18 +50,18 @@ with open(DATA_FILENAME, "w+") as file_out:
     time_start = time.time()
     for i, number in enumerate(gen_numbers(), start=1):       # генерируем число в цикле
         numbers_sum += number                                 # сумма чисел для контроля качества кода!
-        print(f"{number:.2f}", end=SEPARATOR, file=file_out)  # >>> в файл на диск:
+        print(f"{number:f}", end=SEPARATOR, file=file_out)    # >>> в файл на диск:
         # >>> экран
-        print(f"\rЗапись: {i/NUMBERS_AMOUNT_MAX:>4.0%}   {i:>10,d}   сумма: {numbers_sum:.2f}   время: {(time.time()-time_start):.2f} с", end=' '*5)
+        print(f"\rЗапись: {i/NUMBERS_AMOUNT_MAX:>4.0%}   {i:>10,d}   сумма: {numbers_sum:f}   время: {(time.time()-time_start):.2f} с", end=' '*5)
     print(flush=True, file=file_out)                          # сбросим кэш диска
     print()
 
-# Читаем и подсчитываем
+# Читаем и суммируем
 time_start = time.time()
 numbers_sum = 0
 for i, number in enumerate(get_number(DATA_FILENAME), start=1):
     numbers_sum += number
-    # >>> экран:
-    print(f"\rЧтение: {i / NUMBERS_AMOUNT_MAX:>4.0%}   {i:>10,d}   сумма: {numbers_sum:.2f}   время: {(time.time() - time_start):.2f} с",  end=' '*5)
+    # >>> экран
+    print(f"\rЧтение: {i / NUMBERS_AMOUNT_MAX:>4.0%}   {i:>10,d}   сумма: {numbers_sum:f}   время: {(time.time() - time_start):.2f} с",  end=' '*5)
 
 print()
